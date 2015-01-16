31314693b92e7f98e7b8b8bfce7fd131b5ed6bfd
Bruno Lemos <brunohplemos@gmail.com>
2015-01-16 03:52:20 -0200

    MainPage Refactoring

--- Keep/Keep.WindowsPhone/Views/MainPage.xaml.cs
+++ Keep/Keep.WindowsPhone/Views/MainPage.xaml.cs
@@ -2,11 +2,7 @@
 using Keep.Models;
 using Keep.Utils;
 using Keep.ViewModels;
-using System.Collections;
-using System.Collections.ObjectModel;
 using System.Linq;
-using Windows.Phone.UI.Input;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -28,25 +24,19 @@ namespace Keep.Views
             this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
             this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
 
-            HardwareButtons.BackPressed += (s, e) => SaveNotesIfReordered();
-            App.Current.Suspending += (s, e) => SaveNotesIfReordered();
+            EnableReorder();
         }
+
+        partial void EnableReorder();
 
         private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
             App.ChangeStatusBarColor();
             App.RootFrame.Background = LayoutRoot.Background;
-
-            viewModel.ReorderedNotes = false;
         }
 
         private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
         {
-            #if WINDOWS_PHONE_APP
-            NotesListView.ReorderMode = ListViewReorderMode.Disabled;
-            #endif
-
-            SaveNotesIfReordered();
         }
 
         #region NavigationHelper registration
@@ -65,20 +55,6 @@ namespace Keep.Views
 
         #endregion
 
-        private async void SaveNotesIfReordered()
-        {
-            //save notes if reordered
-            if (viewModel.ReorderedNotes)
-                if (await AppData.SaveNotes())
-                    viewModel.ReorderedNotes = false;
-        }
-
-        private void NotesListView_Holding(object sender, Windows.UI.Xaml.Input.HoldingRoutedEventArgs e)
-        {
-            #if WINDOWS_PHONE_APP
-            NotesListView.ReorderMode = ListViewReorderMode.Enabled;
-            #endif
-        }
 
         private void NotesListView_ItemClick(object sender, ItemClickEventArgs e)
         {
@@ -94,33 +70,6 @@ namespace Keep.Views
                 note = AppData.Notes.Where<Note>(n => n.ID == note.ID).FirstOrDefault();
 
             Frame.Navigate(typeof(NoteEditPage), note);
-        }
-
-        private void NotesListView_DragItemsStarting(object sender, DragItemsStartingEventArgs e)
-        {
-            e.Data.Properties["sourceItem"] = e.Items[0];
-        }
-
-        private void NotesListView_DragOver(object sender, DragEventArgs e)
-        {
-            e.Data.Properties["targetItem"] = (e.OriginalSource as FrameworkElement).DataContext;
-        }
-
-        private void NotesListView_Drop(object sender, DragEventArgs e)
-        {
-            var sourceItem = e.Data.Properties["sourceItem"] as Note;
-            var targetItem = e.Data.Properties["targetItem"] as Note;
-            if (sourceItem == null || targetItem == null) return;
-
-            var items = (sender as ItemsControl).ItemsSource as Notes;
-            if (items == null) return;
-
-            int sourceItemIndex = items.IndexOf(sourceItem);
-            int targetItemIndex = items.IndexOf(targetItem);
-            if (sourceItemIndex == targetItemIndex || sourceItemIndex < 0 || targetItemIndex < 0) return;
-
-            viewModel.ReorderedNotes = true;
-            items.Move(sourceItemIndex, targetItemIndex);
         }
     }
 }
---
+++ Keep/Keep.WindowsPhone/Views/Partials/MainPage_NoteReorder.cs
@@ -0,0 +1,81 @@
+﻿using Keep.Common;
+using Keep.Models;
+using Keep.Utils;
+using Windows.Phone.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Keep.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        partial void EnableReorder()
+        {
+            HardwareButtons.BackPressed += (s, e) => SaveNotesIfReordered();
+
+            navigationHelper.LoadState += NavigationHelper_LoadState1;
+            navigationHelper.SaveState += NavigationHelper_SaveState1;
+
+            NotesListView.Holding += NotesListView_Holding;
+            NotesListView.DragItemsStarting += NotesListView_DragItemsStarting;
+            NotesListView.DragOver += NotesListView_DragOver;
+            NotesListView.DragOver += NotesListView_Drop;
+        }
+
+        private void NavigationHelper_LoadState1(object sender, LoadStateEventArgs e)
+        {
+            viewModel.ReorderedNotes = false;
+        }
+
+        private void NavigationHelper_SaveState1(object sender, SaveStateEventArgs e)
+        {
+            #if WINDOWS_PHONE_APP
+            NotesListView.ReorderMode = ListViewReorderMode.Disabled;
+            #endif
+
+            SaveNotesIfReordered();
+        }
+
+        private async void SaveNotesIfReordered()
+        {
+            //save notes if reordered
+            if (viewModel.ReorderedNotes)
+                if (await AppData.SaveNotes())
+                    viewModel.ReorderedNotes = false;
+        }
+
+        private void NotesListView_Holding(object sender, Windows.UI.Xaml.Input.HoldingRoutedEventArgs e)
+        {
+            #if WINDOWS_PHONE_APP
+            NotesListView.ReorderMode = ListViewReorderMode.Enabled;
+#endif
+        }
+
+        private void NotesListView_DragItemsStarting(object sender, DragItemsStartingEventArgs e)
+        {
+            e.Data.Properties["sourceItem"] = e.Items[0];
+        }
+
+        private void NotesListView_DragOver(object sender, DragEventArgs e)
+        {
+            e.Data.Properties["targetItem"] = (e.OriginalSource as FrameworkElement).DataContext;
+        }
+
+        private void NotesListView_Drop(object sender, DragEventArgs e)
+        {
+            var sourceItem = e.Data.Properties["sourceItem"] as Note;
+            var targetItem = e.Data.Properties["targetItem"] as Note;
+            if (sourceItem == null || targetItem == null) return;
+
+            var items = (sender as ItemsControl).ItemsSource as Notes;
+            if (items == null) return;
+
+            int sourceItemIndex = items.IndexOf(sourceItem);
+            int targetItemIndex = items.IndexOf(targetItem);
+            if (sourceItemIndex == targetItemIndex || sourceItemIndex < 0 || targetItemIndex < 0) return;
+
+            viewModel.ReorderedNotes = true;
+            items.Move(sourceItemIndex, targetItemIndex);
+        }
+    }
+}
