616ce72e744bca9ffdee6438d0a9dc05f328aa29
Bruno Lemos <brunohplemos@gmail.com>
2015-05-02 15:48:40 -0300

    Small Folder Refactoring

--- FlatNotes.Windows81/Properties/AssemblyInfo.cs
+++ FlatNotes.Windows81/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FlatNotes.Windows")]
+[assembly: AssemblyTitle("FlatNotes.Windows81")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FlatNotes.Windows")]
+[assembly: AssemblyProduct("FlatNotes.Windows81")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
