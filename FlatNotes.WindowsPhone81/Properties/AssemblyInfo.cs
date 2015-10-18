c8b99510c6d4b60beb1565d6e854877a7bf54ff4
Bruno Lemos <brunohplemos@gmail.com>
2015-10-18 02:49:04 -0200

    Recreated projects to fix certificate issue

--- FlatNotes.WindowsPhone81/Properties/AssemblyInfo.cs
+++ FlatNotes.WindowsPhone81/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FlatNotes.WindowsPhone81")]
+[assembly: AssemblyTitle("Flat Notes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FlatNotes.WindowsPhone81")]
+[assembly: AssemblyProduct("Flat Notes")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -27,3 +28,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
+
